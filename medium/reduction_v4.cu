1b0e71aa7d89b5aa89781b3050bf0bad3d21d912
WuXiaofan <158270233+FangehaPlus@users.noreply.github.com>
2026-04-26 20:43:08 +0800

    Create reduction_v4.cu

---
+++ medium/reduction_v4.cu
@@ -0,0 +1,52 @@
+#include <cuda_runtime.h>
+
+#define THREADS_PER_BLOCK 256
+
+/*
+Reduction.
+Unroll the last wrap.
+*/
+
+__global__ void reduction(const float* input, float* output, int N) {
+    volatile __shared__ float sram[THREADS_PER_BLOCK];
+    /*
+    `volatile` tells the compiler to access shared memory directly instead of optimizing accesses via registers.
+    */
+    int glob_offset = blockDim.x * blockIdx.x * 2;
+    int idx_sram = threadIdx.x;
+    sram[idx_sram] = (glob_offset + idx_sram) < N ? input[glob_offset + idx_sram] : 0.0f;
+    sram[idx_sram] += (glob_offset + idx_sram + blockDim.x) < N ? input[glob_offset + idx_sram + blockDim.x] : 0.0f;
+    __syncthreads();
+
+    for (int i = blockDim.x / 2; i > 32; i >>= 1) {
+        if (idx_sram < i) {
+            sram[idx_sram] += sram[idx_sram + i];
+        }
+        __syncthreads();
+    }
+
+    if (idx_sram < 32) {
+        sram[idx_sram] += sram[idx_sram + 32];
+        sram[idx_sram] += sram[idx_sram + 16];
+        sram[idx_sram] += sram[idx_sram + 8];
+        sram[idx_sram] += sram[idx_sram + 4];
+        sram[idx_sram] += sram[idx_sram + 2];
+        sram[idx_sram] += sram[idx_sram + 1];
+    }
+
+    if (idx_sram == 0) {
+        atomicAdd(output, sram[0]);
+    }
+}
+
+// input, output are device pointers
+extern "C" void solve(const float* input, float* output, int N) {
+    dim3 threadsPerBlock(THREADS_PER_BLOCK);
+
+    // it should be ceil[N / (THREADS_PER_BLOCK * 2)]
+    dim3 blocksPerGrid((N + threadsPerBlock.x * 2 - 1) / threadsPerBlock.x * 2);
+
+    reduction<<<blocksPerGrid, threadsPerBlock>>>(input, output, N);
+    
+}
+
